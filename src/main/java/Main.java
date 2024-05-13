02caa5eac3cf28704fd6d91c8330b9794fe5d899
Samuel Vitor Mariano <169068157+Samuel123450908@users.noreply.github.com>
2024-05-13 22:28:11 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("------------");
+    System.out.println("brooklyn 99");
+    System.out.println("------------");
+     System.out.println("");
+    System.out.println("O despreocupado detetive Jake Peralta precisa aprender a seguir as regras e trabalhar em equipe quando um capitão exigente assume o comando de seu esquadrão");
+    System.out.println("");
+    System.out.println("Link do trailer de blooklyn 99");
+    System.out.println("");
+     System.out.println("-----------------------------------------------");
+    System.out.println("https://youtu.be/i69rgj0s9fw?si=TRzNOuk_xdqOAyzy");
+    System.out.println("-----------------------------------------------");
+}
+}
